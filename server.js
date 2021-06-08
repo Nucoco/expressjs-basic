c2594f1fe62f524d8d9f444b54c1128f0913e815
Nucoco <yuuta11511997@gmail.com>
2021-06-08 09:47:09 +0900

    Add a process fo post requests

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const port = process.env.port || process.env.PORT || 5000;
 const apiRoot = "/api";
 
 const app = express();
+
 // configure app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,10 +22,69 @@ app.use(bodyParser.json());
 app.use(cors({ origin: /http:\/\/localhost/ }));
 app.options("*", cors());
 
+// sample database
+const db = {
+	christopher: {
+		user: "christopher",
+		currency: "USD",
+		balance: 100,
+		description: "A sample account",
+		transaction: [],
+	},
+};
+
 // configure routes
 const router = express.Router();
 router.get("/", (req, res) => {
 	res.send(`${package.description} - v${package.version}`);
+});
+
+router.get("/accounts/:user", (req, res) => {
+	const user = req.params.user;
+	const account = db[user];
+
+	if (!account) {
+		return res.status(404).json({ error: "User does not exist" });
+	}
+	// default status code is 200
+	return res.json(account);
+});
+
+router.post("/accounts", (req, res) => {
+	// Normally at first, parse request object including json payload sent from client. But it's done by body-parser in above.
+	const body = req.body;
+
+	// validate required values
+	if (!body.user || !body.currency) {
+		return res.status(400).json({ error: "user and currency are required" });
+	}
+
+	// check account doesn't exits
+	if (db[body.user]) {
+		return res.status(400).json({ error: "Account already exists" });
+	}
+
+	// balance
+	let balance = body.balance;
+	if (balance && typeof balance !== "number") {
+		balance = parseFloat(balance);
+		if (isNaN(balance)) {
+			return res.status(400).json({ error: "balance must be a number" });
+		}
+	}
+
+	// now we can create the account!
+	const account = {
+		user: body.user,
+		currency: body.currency,
+		description: body.description || `${body.user}'s account`,
+		balance: balance || 0,
+		transaction: [],
+	};
+	db[account.user] = account;
+
+	// return the account
+	return res.status(201).json(account);
 });
 
 // register all our routes
